2e8a9f73f652d74251172472e9245c907ae76002
vishnu <vvr947@gmail.com>
2020-12-19 00:11:59 +0530

    method fixed

--- js/args.js
+++ js/args.js
@@ -1,10 +1,11 @@
 var kv = tasks.get('kv');
 
-print("checking args",new Date().toISOString().split("T")[0])
 
-kv.putString("date",new Date().toISOString().split("T")[0])
-var count = kv.getLong('count') || 0
-kv.putLong("count",count);
+var date = new Date().toISOString().split("T")[0];
+print("date is",date)
+kv.putString("date",date)
+// var count = kv.getLong('count') || 0
+// kv.putLong("count",count);
 
-print("date is",kv.getString('date'));
-print("count",kv.getLong('count'))
+// print("date is",kv.getString('date'));
+// print("count",kv.getLong('count'))
