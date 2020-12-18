95c2023b777feb18e19c199a41c723bcd5f4d5e7
vishnu <vvr947@gmail.com>
2020-12-19 00:08:03 +0530

    method fixed

--- js/args.js
+++ js/args.js
@@ -4,7 +4,7 @@ print("checking args",new Date().toISOString().split("T")[0])
 
 kv.putString("date",new Date().toISOString().split("T")[0])
 var count = kv.getLong('count') || 0
-kv.putInteger("count",count);
+kv.putLong("count",count);
 
 print("date is",kv.getString('date'));
 print("count",kv.getLong('count'))
