114796dd158ca725a3ea5a16e1ccde971693636f
vishnu <vvr947@gmail.com>
2020-12-19 01:19:43 +0530

    js added

--- js/args.js
+++ js/args.js
@@ -2,11 +2,40 @@ var kv = tasks.get('kv');
 
 print("js script")
 
-var date = new Date().toISOString().split("T")[0];
-print("date is",date)
-kv.putString(execution,"date",date)
-var count = kv.getLong(execution,'count') || 0
-kv.putLong(execution,"count",count);
 
-print("date is",kv.getString(execution,'date'));
-print("count",kv.getLong(execution,'count'))
+
+function getDateString(){
+    return new Date().toISOString().split("T")[0]
+}
+
+var storedDate = kv.getString(execution,'date');
+var storedCount = kv.getLong(execution,'count');
+
+var today = getDateString();
+
+
+if(!storedDate){
+    print("no date found may this is first run setting date" + getDateString());
+    kv.putString(execution,'date',today)
+    kv.putLong(execution,'count',0)
+}
+
+
+
+if(storedDate && storedDate === today){
+    print("number of processes ran today" + storedCount)
+    kv.putLong(execution,'count',storedCount+1)
+    if(storedCount > 2){
+        print("limit reached")
+        return;
+    }
+}
+
+if(storedDate && storedDate !== today ){
+    print("This is new day")
+    kv.putString(execution,'date',today)
+    kv.putLong(execution,'count',0)
+}
+
+print('date is' + kv.getString(execution,'date'));
+print('count',kv.getLong(execution,'count'))
